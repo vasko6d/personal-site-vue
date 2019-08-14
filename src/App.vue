605f4bd0a7c65ea63ddba21581e014f2fa56789a
David Vasko <vasko6d@gmail.com>
2019-08-14 15:12:02 -0700

    adding comment for fixing jumping scrollbar issue

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-left: calc(100vw - 100%);
+  margin-left: calc(100vw - 100%); /* fixes jumping scrollbar issue */
 }
 #nav {
   padding: 30px;
