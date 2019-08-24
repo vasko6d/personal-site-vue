92b17eba0125ff1ff64cc7ef793fa796754df045
David Vasko <vasko6d@gmail.com>
2019-08-23 23:51:02 -0700

    rewording of finite volume page

--- src/views/FiniteVolume.vue
+++ src/views/FiniteVolume.vue
@@ -5,22 +5,19 @@
       <img src="../assets/img/mach1x.png" />
       <div class="blk">
         <p>
-          This is a computational project I completed durring my Masters program
-          at UCLA in the class "250D: Computational Aerodynamics." I developed a
-          two dimensional finite volume solver for a supersonic jet inlet.
-          MacCormacks Method with artificial dissipation was utilized. Please
-          view the
+          I developed a two dimensional finite volume solver for a supersonic
+          jet inlet. MacCormacks Method with artificial dissipation was
+          utilized. Please view the
           <a href="pdf/2d-supersonic-jet-inlet-presentation.pdf"
             >presentation</a
           >
           , the
           <a href="pdf/2d-supersonic-jet-inlet-report.pdf">full report</a> and
           checkout the source code on my github page to see the awesomeness.
-          This project was done using MATLAB R2015a Student Version. The
-          following figure is sample of the results section showing how the mach
-          number drops abruptly across the oblique shocks. These oblique shocks
-          are used intentionaly in jet engine inlets to slow down supersonic
-          air.
+          This project was done using MATLAB R2015a Student Version. The above
+          figure is sample of the results section showing how the mach number
+          drops abruptly across the oblique shocks. These oblique shocks are
+          used intentionaly in jet engine inlets to slow down supersonic air.
         </p>
       </div>
     </div>
