d8ca8976a5f067c3277d1605e41f3e06ba356cdd
David Vasko <vasko6d@gmail.com>
2019-09-03 09:47:42 -0700

    making default controls adn ability to pass custom keybinds

--- src/components/webgl/WebglCamera.vue
+++ src/components/webgl/WebglCamera.vue
@@ -11,49 +11,49 @@
           class="u-item cbtn prm"
           v-on:click="control(controls.move.forward.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.move.forward.icon">{{
-            keybindToggle ? controls.move.forward.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.move.forward.icon">
+            {{ keybindToggle ? controls.move.forward.keybind : "" }}
+          </i>
         </div>
         <div
           class="d-item cbtn prm"
           v-on:click="control(controls.move.backward.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.move.backward.icon">{{
-            keybindToggle ? controls.move.backward.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.move.backward.icon">
+            {{ keybindToggle ? controls.move.backward.keybind : "" }}
+          </i>
         </div>
         <div
           class="l-item cbtn prm"
           v-on:click="control(controls.move.left.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.move.left.icon">{{
-            keybindToggle ? controls.move.left.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.move.left.icon">
+            {{ keybindToggle ? controls.move.left.keybind : "" }}
+          </i>
         </div>
         <div
           class="r-item cbtn prm"
           v-on:click="control(controls.move.right.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.move.right.icon">{{
-            keybindToggle ? controls.move.right.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.move.right.icon">
+            {{ keybindToggle ? controls.move.right.keybind : "" }}
+          </i>
         </div>
         <div
           class="ul-item cbtn scnd"
           v-on:click="control(controls.move.up.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.move.up.icon">{{
-            keybindToggle ? controls.move.up.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.move.up.icon">
+            {{ keybindToggle ? controls.move.up.keybind : "" }}
+          </i>
         </div>
         <div
           class="ur-item cbtn scnd"
           v-on:click="control(controls.move.down.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.move.down.icon">{{
-            keybindToggle ? controls.move.down.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.move.down.icon">
+            {{ keybindToggle ? controls.move.down.keybind : "" }}
+          </i>
         </div>
       </div>
       <div class="udlr-group mr">
@@ -62,49 +62,49 @@
           class="u-item cbtn prm"
           v-on:click="control(controls.look.up.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.look.up.icon">{{
-            keybindToggle ? controls.look.up.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.look.up.icon">
+            {{ keybindToggle ? controls.look.up.keybind : "" }}
+          </i>
         </div>
         <div
           class="d-item cbtn prm"
           v-on:click="control(controls.look.down.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.look.down.icon">{{
-            keybindToggle ? controls.look.down.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.look.down.icon">
+            {{ keybindToggle ? controls.look.down.keybind : "" }}
+          </i>
         </div>
         <div
           class="l-item cbtn prm"
           v-on:click="control(controls.look.left.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.look.left.icon">{{
-            keybindToggle ? controls.look.left.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.look.left.icon">
+            {{ keybindToggle ? controls.look.left.keybind : "" }}
+          </i>
         </div>
         <div
           class="r-item cbtn prm"
           v-on:click="control(controls.look.right.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.look.right.icon">{{
-            keybindToggle ? controls.look.right.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.look.right.icon">
+            {{ keybindToggle ? controls.look.right.keybind : "" }}
+          </i>
         </div>
         <div
           class="ul-item cbtn scnd"
           v-on:click="control(controls.look.zoomin.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.look.zoomin.icon">{{
-            keybindToggle ? controls.look.zoomin.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.look.zoomin.icon">
+            {{ keybindToggle ? controls.look.zoomin.keybind : "" }}
+          </i>
         </div>
         <div
           class="ur-item cbtn scnd"
           v-on:click="control(controls.look.zoomout.keybind, camera)"
         >
-          <i :class="keybindToggle ? '' : controls.look.zoomout.icon">{{
-            keybindToggle ? controls.look.zoomout.keybind : ""
-          }}</i>
+          <i :class="keybindToggle ? '' : controls.look.zoomout.icon">
+            {{ keybindToggle ? controls.look.zoomout.keybind : "" }}
+          </i>
         </div>
       </div>
     </div>
@@ -128,24 +128,7 @@ export default {
   data() {
     return {
       keybindToggle: false,
-      controls: {
-        move: {
-          forward: { keybind: "w", icon: "fa fa-caret-up" },
-          backward: { keybind: "s", icon: "fa fa-caret-down" },
-          left: { keybind: "a", icon: "fa fa-caret-left" },
-          right: { keybind: "d", icon: "fa fa-caret-right" },
-          up: { keybind: "q", icon: "fa fa-arrow-up" },
-          down: { keybind: "e", icon: "fa fa-arrow-down" }
-        },
-        look: {
-          up: { keybind: "u", icon: "fa fa-caret-up" },
-          down: { keybind: "j", icon: "fa fa-caret-down" },
-          left: { keybind: "h", icon: "fa fa-caret-left" },
-          right: { keybind: "k", icon: "fa fa-caret-right" },
-          zoomin: { keybind: "y", icon: "fa fa-plus" },
-          zoomout: { keybind: "i", icon: "fa fa-minus" }
-        }
-      }
+      controls: this.defaultControls()
     };
   },
   methods: {
@@ -162,27 +145,53 @@ export default {
       };
       return camera;
     },
-    control(ch, camera) {
+    defaultControls() {
+      var controls = {
+        move: {
+          forward: { keybind: "w", icon: "fa fa-caret-up" },
+          backward: { keybind: "s", icon: "fa fa-caret-down" },
+          left: { keybind: "a", icon: "fa fa-caret-left" },
+          right: { keybind: "d", icon: "fa fa-caret-right" },
+          up: { keybind: "q", icon: "fa fa-arrow-up" },
+          down: { keybind: "e", icon: "fa fa-arrow-down" }
+        },
+        look: {
+          up: { keybind: "u", icon: "fa fa-caret-up" },
+          down: { keybind: "j", icon: "fa fa-caret-down" },
+          left: { keybind: "h", icon: "fa fa-caret-left" },
+          right: { keybind: "k", icon: "fa fa-caret-right" },
+          zoomin: { keybind: "y", icon: "fa fa-plus" },
+          zoomout: { keybind: "i", icon: "fa fa-minus" }
+        }
+      };
+      return controls;
+    },
+    control(ch, camera, controls) {
       // Make sure character "ch" is lowercase
       ch = ch.toLowerCase();
 
+      // Make default controls if no controls were passed
+      if (!controls) {
+        controls = this.defaultControls();
+      }
+
       // Increas or decreas azimuth with h and k (look left or right)
-      if (ch === this.controls.look.left.keybind) {
+      if (ch === controls.look.left.keybind) {
         camera.phi += camera.dr;
         return;
       }
-      if (ch === this.controls.look.right.keybind) {
+      if (ch === controls.look.right.keybind) {
         camera.phi -= camera.dr;
         return;
       }
       // Increase/decrease Altitude with u and j (loop up or down)
-      if (ch === this.controls.look.up.keybind) {
+      if (ch === controls.look.up.keybind) {
         if (camera.theta < 1.55) {
           camera.theta += camera.dr;
         }
         return;
       }
-      if (ch === this.controls.look.down.keybind) {
+      if (ch === controls.look.down.keybind) {
         if (camera.theta > -1.55) {
           camera.theta -= camera.dr;
         }
@@ -190,11 +199,11 @@ export default {
       }
 
       // Widen or narrow the verticle feild of view with g or b (zoom in or zoom out essentially)
-      if (ch === this.controls.look.zoomout.keybind) {
+      if (ch === controls.look.zoomout.keybind) {
         camera.fovy += 1;
         return;
       }
-      if (ch === this.controls.look.zoomin.keybind) {
+      if (ch === controls.look.zoomin.keybind) {
         camera.fovy -= 1;
         return;
       }
@@ -213,7 +222,7 @@ export default {
 
       // Using the orthonormal basis, move forward, backward, left, right, up and down
       // Left
-      if (ch === this.controls.move.left.keybind) {
+      if (ch === controls.move.left.keybind) {
         camera.translation = mv.mult(
           camera.translation,
           mv.translationMatrix(
@@ -226,7 +235,7 @@ export default {
         );
       }
       // Backward
-      if (ch === this.controls.move.backward.keybind) {
+      if (ch === controls.move.backward.keybind) {
         camera.translation = mv.mult(
           camera.translation,
           mv.translationMatrix(
@@ -239,7 +248,7 @@ export default {
         );
       }
       // Right
-      if (ch === this.controls.move.right.keybind) {
+      if (ch === controls.move.right.keybind) {
         camera.translation = mv.mult(
           camera.translation,
           mv.translationMatrix(
@@ -252,7 +261,7 @@ export default {
         );
       }
       // Forward
-      if (ch === this.controls.move.forward.keybind) {
+      if (ch === controls.move.forward.keybind) {
         camera.translation = mv.mult(
           camera.translation,
           mv.translationMatrix(
@@ -265,7 +274,7 @@ export default {
         );
       }
       // Upward
-      if (ch === this.controls.move.up.keybind) {
+      if (ch === controls.move.up.keybind) {
         camera.translation = mv.mult(
           camera.translation,
           mv.translationMatrix(
@@ -278,7 +287,7 @@ export default {
         );
       }
       // Downward
-      if (ch === this.controls.move.down.keybind) {
+      if (ch === controls.move.down.keybind) {
         camera.translation = mv.mult(
           camera.translation,
           mv.translationMatrix(
