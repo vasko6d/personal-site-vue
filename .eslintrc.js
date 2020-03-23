44640a1135a67ed5adeed76e53f07f604981caa8
David Vasko <vasko6d@gmail.com>
2020-03-23 12:43:50 -0700

    Remove production error on colsole statements, allow in esLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,8 @@ module.exports = {
     "@vue/prettier",
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": "off",//process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": "off",//process.env.NODE_ENV === "production" ? "error" : "off",
   },
   parserOptions: {
     parser: "babel-eslint",
