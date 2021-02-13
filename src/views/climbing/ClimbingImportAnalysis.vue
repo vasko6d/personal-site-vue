a00f77da775b7e193f327f25afa04df19148fc6f
David Vasko <vasko6d@gmail.com>
2021-02-13 13:13:51 -0800

    adding optional reccommend file

--- src/views/climbing/ClimbingImportAnalysis.vue
+++ src/views/climbing/ClimbingImportAnalysis.vue
@@ -4,9 +4,15 @@
       <div class="blk-container">
         <h1>Analysis Importer</h1>
         <div class="bg1">
-          <p>
+          <p style="text-align: left;">
             Select a local JSON file of ascents from 8a.nu to view the analytics
-            of a climber not in the sandbox
+            of a climber not in the sandbox. Get it by using the following url
+            when logged into 8a.nu:
+          </p>
+          <p style="font-size: 10pt;">
+            https://www.8a.nu/api/users/<strong>{user-name}</strong>/ascents?category=bouldering&pageIndex=0&pageSize=<strong
+              >{total-ascents}</strong
+            >
           </p>
           <div class="import-btn">
             <input
@@ -15,6 +21,25 @@
               accept=".json"
               @change="onFileSelect($event)"
             />
+          </div>
+          <div v-show="this.ascentJsonFile">
+            <p style="text-align: left;">
+              Optionally select a local recommend json file. Get it my using the
+              following url when logged into 8a.nu:
+            </p>
+            <p style="font-size: 10pt;">
+              https://www.8a.nu/api/users/<strong>{user-name}</strong>/recommended?pageSize=<strong
+                >{total-recommended}</strong
+              >
+            </p>
+            <div class="import-btn">
+              <input
+                type="file"
+                ref="recommendFile"
+                accept=".json"
+                @change="onrecommendFileSelect($event)"
+              />
+            </div>
           </div>
           <div
             v-if="ascentJsonFile && !initialized"
@@ -48,6 +73,8 @@ export default {
     return {
       ascentJsonFile: undefined,
       ascentJson: undefined,
+      recommendFile: undefined,
+      recommendJson: undefined,
       climberName: false,
       importError: undefined,
       initialized: false,
@@ -58,6 +85,13 @@ export default {
       this.climberName = undefined;
       this.initialized = false;
       this.ascentJsonFile = event.srcElement.value;
+      this.recommendFile = undefined;
+      this.$refs.recommendFile.value = null;
+    },
+    onrecommendFileSelect(event) {
+      this.climberName = undefined;
+      this.initialized = false;
+      this.recommendFile = event.srcElement.value;
     },
     onAnalyze() {
       let reader = new FileReader();
@@ -70,13 +104,47 @@ export default {
           this.ascentJson.ascents.length &&
           this.ascentJson.totalItems
         ) {
-          this.climberName = this.ascentJson.ascents[0].userName;
-          console.log(
-            `Sucesfully parsed local JSON file for climber [${this.climberName}], with [${this.ascentJson.ascents.length}] climbs`
-          );
+          if (this.recommendFile) {
+            let reader2 = new FileReader();
+            reader2.readAsText(this.$refs.recommendFile.files[0], "UTF-8");
+            reader2.onload = (evt) => {
+              this.recommendJson = JSON.parse(evt.target.result);
+              if (this.recommendJson) {
+                const recommendMap = {};
+                const getClimbKey = (ascent) => {
+                  return (
+                    (ascent["areaSlug"] || "area-blank") +
+                    ascent["cragSlug"] +
+                    ascent["sectorSlug"] +
+                    ascent["zlaggableSlug"]
+                  );
+                };
+                this.recommendJson.ascents.forEach((ascent) => {
+                  recommendMap[getClimbKey(ascent)] = true;
+                });
+                this.ascentJson.ascents.forEach((ascent) => {
+                  if (recommendMap[getClimbKey(ascent)]) {
+                    ascent["recommend"] = true;
+                  }
+                });
+                this.climberName = this.ascentJson.ascents[0].userName;
+                console.log(
+                  `Sucesfully parsed local JSON file for climber [${this.climberName}] and recommend Data, with [${this.ascentJson.ascents.length}] climbs`
+                );
+              } else {
+                this.importError = "Bad recommend File";
+                alert(this.importError);
+              }
+            };
+          } else {
+            this.climberName = this.ascentJson.ascents[0].userName;
+            console.log(
+              `Sucesfully parsed local JSON file for climber [${this.climberName}], with [${this.ascentJson.ascents.length}] climbs`
+            );
+          }
         } else {
-          this.importError = "Bad File";
-          alert("Bad Json File");
+          this.importError = "Bad Ascent File";
+          alert(this.importError);
         }
       };
       reader.onerror = (evt) => {
