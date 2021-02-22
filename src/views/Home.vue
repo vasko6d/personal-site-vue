1bbfd344dc6f5af3866bc3d8e83be84169b1bf9f
David Vasko <vasko6d@gmail.com>
2021-02-22 13:29:40 -0800

    updating home photo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home">
-    <img alt="David Vasko Portrait" src="../assets/img/vasko-nextgate.jpg" />
+    <img alt="David Vasko Portrait" src="../assets/img/20210214-david.jpeg" />
   </div>
 </template>
 
